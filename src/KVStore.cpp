6c17a5dbae519751baaf4889ff2ce0b3ba89e746
Vignesh S <vigneshsekar255.ai@gmail.com>
2026-02-13 22:12:02 +0530

    Update KVStore.cpp

--- src/KVStore.cpp
+++ src/KVStore.cpp
@@ -1,16 +1,53 @@
 #include "KVStore.h"
 
+KVStore::KVStore(size_t cap) : capacity(cap) {}
+
 void KVStore::set(const std::string& key, const std::string& value) {
-    store[key] = value;
+
+    // If key exists → update + move to front
+    if (cacheMap.find(key) != cacheMap.end()) {
+        cacheList.erase(cacheMap[key]);
+    }
+    // If capacity reached → evict LRU
+    else if (cacheList.size() >= capacity) {
+        auto lru = cacheList.back();
+        cacheMap.erase(lru.first);
+        cacheList.pop_back();
+    }
+
+    cacheList.push_front({key, value});
+    cacheMap[key] = cacheList.begin();
 }
 
 std::string KVStore::get(const std::string& key) {
-    if (store.find(key) == store.end()) {
+
+    if (cacheMap.find(key) == cacheMap.end())
         return "NULL";
-    }
-    return store[key];
+
+    auto node = cacheMap[key];
+    std::string value = node->second;
+
+    // Move accessed key to front
+    cacheList.erase(node);
+    cacheList.push_front({key, value});
+    cacheMap[key] = cacheList.begin();
+
+    return value;
 }
 
 bool KVStore::del(const std::string& key) {
-    return store.erase(key) > 0;
+
+    if (cacheMap.find(key) == cacheMap.end())
+        return false;
+
+    cacheList.erase(cacheMap[key]);
+    cacheMap.erase(key);
+    return true;
+}
+
+void KVStore::display() {
+    for (auto &p : cacheList) {
+        std::cout << "(" << p.first << ":" << p.second << ") ";
+    }
+    std::cout << std::endl;
 }
